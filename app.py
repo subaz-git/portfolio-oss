dae52fccfb8a2c79fb79dbd108ef08ebf38cdd6a
Subash S <iamsubash2006@gmail.com>
2026-01-16 12:49:26 +0530

    converted flask to fastapi

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask,render_template,jsonify
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
-app=Flask(__name__)
+app = FastAPI()
 
-@app.route('/')
-def home():
-    return render_template("index.html")
+templates = Jinja2Templates(directory="templates")
 
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
-if __name__=="__main__":
-    app.run(debug=true)
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
